5cffb746bc0732b7b8896241b06db9ec68d7aad4
Stìophan A. Horne <steven.a.horne@btinternet.com>
2020-05-25 20:20:58 +0000

    Added fields to art>models.py for artworks

--- art/models.py
+++ art/models.py
@@ -2,9 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Work(models.Model):
-    
-    name = models.CharField(max_length=30, blank=False)
-    done = models.BooleanField(blank=False, default=False)
+
+    title = models.CharField(max_length=30, blank=False)
+    category = models.CharField(max_length=30, blank=False)
+    image = models.ImageField(upload_to="images", blank=True, null=False)
+    surface = models.CharField(max_length=30, blank=False, default='')
+    media = models.CharField(max_length=30, blank=False, default='')
+    category = models.CharField(max_length=30, blank=False, default='')
+    hsize = models.IntegerField(blank=False, null=False, default=0)
+    vsize = models.IntegerField(blank=False, null=False, default=0)
+    price = models.DecimalField(max_digits=6, decimal_places=2, blank=False, default=0)
 
     def __str__(self):
         return self.name
