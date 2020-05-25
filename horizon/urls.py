d7334c239b987bdf39c69a50c681a7e89b4ce2b8
Stìophan A. Horne <steven.a.horne@btinternet.com>
2020-05-25 14:32:42 +0000

    Created art app, added urls and views, tested in browser - all OK

--- horizon/urls.py
+++ horizon/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from art.views import say_hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('art/', say_hello)
 ]
