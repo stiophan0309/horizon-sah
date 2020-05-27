2920b6dfce40083ecf463ba704541c3e912ae5ab
Stìophan A. Horne <steven.a.horne@btinternet.com>
2020-05-27 21:26:51 +0000

    Added redirect to index page for logged in users

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
 from accounts.forms import UserLoginForm
 
 # Create your views here.
@@ -16,17 +17,19 @@ def logout(request):
 
 def login(request):
     """Return a login page"""
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
     if request.method == "POST":
         login_form = UserLoginForm(request.POST)
 
         if login_form.is_valid():
             user = auth.authenticate(username=request.POST['username'],
                                     password=request.POST['password'])
-            
+            messages.success(request, "You have successfully logged in!")
 
             if user:
                 auth.login(user=user, request=request)
-                messages.success(request, "You have successfully logged in!")
+                return redirect(reverse('index'))
             else:
                 login_form.add_error(None, "Your username or password is incorrect")
     else:
