09467971aca4b100d9934734c6c12d9731a09f83
Stìophan A. Horne <steven.a.horne@btinternet.com>
2020-05-30 15:31:22 +0000

    Added Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn horizon.wsgi:application
--- env.py
+++
@@ -1,4 +0,0 @@
-import os
-
-os.environ.setdefault("DATABASE_URL", "postgres://atuorddzkugxip:a586ba961df404c48fb340ecb7bfd48f18ce0aefa6f9e41421e983ee31a92d5b@ec2-54-217-204-34.eu-west-1.compute.amazonaws.com:5432/dc2f51jb40gqsq")
-os.environ.setdefault("SECRET_KEY", "7p4!=n72cxhaeyf0aq7sxgb03jmxpvfm8xigsw#4n((amj01w&")
