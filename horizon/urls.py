d809459e13611dea798ced07f145ded38a70e908
Stìophan A. Horne <steven.a.horne@btinternet.com>
2020-05-30 21:23:31 +0000

    Added Index page to Home and added basic content

--- horizon/urls.py
+++ horizon/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from accounts.views import index
+from home.views import index
 from accounts import urls as accounts_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', index, name='index'),
     url(r'^accounts/', include(accounts_urls))
 ]
