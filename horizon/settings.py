40f576aea6c22b1444f6373af56174bfe371de2b
Stìophan A. Horne <steven.a.horne@btinternet.com>
2020-05-30 15:45:04 +0000

    Amended app name for  Heroku app to Allowed Hosts

--- horizon/settings.py
+++ horizon/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.gitpod.io', 'https://horizon-sah.herokuapp.com']
+ALLOWED_HOSTS = ['.gitpod.io', 'horizon-sah.herokuapp.com']
 
 
 # Application definition
