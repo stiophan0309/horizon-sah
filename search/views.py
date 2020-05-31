e8dab8bd700ec00af6a522dfe6718e8b63a1ae9b
Stìophan A. Horne <steven.a.horne@btinternet.com>
2020-05-31 21:15:28 +0000

    Added Search App and craeted view

---
+++ search/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from works.models import Work
+
+# Create your views here.
+def do_search(request):
+    works = Work.objects.filter(name__icontains=request.GET['q'])
+    return render(request, "works.html", {"works": works})
