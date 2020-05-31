893b7a646ee732d41679dada6dfc0aa5727dcd16
Stìophan A. Horne <steven.a.horne@btinternet.com>
2020-05-31 14:04:44 +0000

    Added models and views for works app

--- horizon/settings.py
+++ horizon/settings.py
@@ -141,6 +141,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
--- horizon/urls.py
+++ horizon/urls.py
@@ -17,9 +17,15 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from home.views import index
 from accounts import urls as accounts_urls
+from works import urls as urls_works
+from works.views import all_works
+from django.views import static
+from .settings import MEDIA_ROOT
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', index, name='index'),
-    url(r'^accounts/', include(accounts_urls))
+    url(r'^accounts/', include(accounts_urls)),
+    url(r'^works/', include(urls_works)),
+    url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
 ]
