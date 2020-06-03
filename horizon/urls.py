a19efe3220c311f2b5b8ef86a4a4174d6d901069
Stìophan A. Horne <steven.a.horne@btinternet.com>
2020-06-03 20:48:02 +0000

    Added checkout views and urls and added Stripe and related JS

--- horizon/urls.py
+++ horizon/urls.py
@@ -21,6 +21,7 @@ from works import urls as urls_works
 from works.views import all_works
 from cart import urls as urls_cart
 from search import urls as urls_search
+from checkout import urls as urls_checkout
 from django.views import static
 from .settings import MEDIA_ROOT
 
@@ -31,6 +32,7 @@ urlpatterns = [
     url(r'^works/', include(urls_works)),
     url(r'^accounts/', include(urls_accounts)),
     url(r'^cart/', include(urls_cart)),
+    url(r'^checkout/', include(urls_checkout)),
     url(r'^search/', include(urls_search)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
 ]
