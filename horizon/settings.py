c175e73da627b036250eafa9cd4e30d80348b93e
Stìophan A. Horne <steven.a.horne@btinternet.com>
2020-06-03 19:27:43 +0000

    Added Checkout app and created model

--- horizon/settings.py
+++ horizon/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'accounts',
     'works',
     'cart',
+    'checkout',
 ]
 
 MIDDLEWARE = [
@@ -146,6 +147,9 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
+STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
