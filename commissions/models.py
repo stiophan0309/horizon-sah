4974fbdb05688b313f2e46d7eab24c3e11224613
Stìophan A. Horne <steven.a.horne@btinternet.com>
2020-06-18 21:55:51 +0000

    Amended datafields in Commissions>Models

--- commissions/models.py
+++ commissions/models.py
@@ -1,14 +1,37 @@
 from django.db import models
 
 # Create your models here.
+STATUS_CHOICES = (
+    ('new','NEW'),
+    ('pending', 'PENDING'),
+    ('in progress','IN PROGRESS'),
+    ('complete','COMPLETE'),
+)
+
+MEDIA_CHOICES = (
+    ('pastel', 'PASTEL'),
+    ('acrylic', 'ACRYLIC'),
+    ('oil','OIL'),
+    ('other','OTHER'),
+)
+
+SURFACE_CHOICES = (
+    ('paper','PAPER'),
+    ('canvas', 'CANVAS'),
+    ('canvas board','CANVAS BOARD'),
+    ('other','OTHER'),
+)
+
 class Item(models.Model):
 
-    name = models.CharField(max_length=30, blank=False)
-    done = models.BooleanField(blank=False, default=False)
-    media = models.CharField(max_length=30, blank=False)
-    surface = models.CharField(max_length=30, blank=False)
+    name = models.CharField(max_length=254, blank=False)
+    email = models.EmailField(max_length=254, blank=False)
+    date = models.DateField(auto_now=True, auto_now_add=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='new')
+    media = models.CharField(max_length=10, choices=MEDIA_CHOICES, default='pastel')
+    surface = models.CharField(max_length=10, choices=SURFACE_CHOICES, default='paper')
     size = models.CharField(max_length=30, blank=False)
-    details = models.CharField(max_length=30, blank=False)
+    details = models.TextField(blank=False)
 
     def __str__(self):
         """ makes readable description in admin panel """
