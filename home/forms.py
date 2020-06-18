f524e45fa5cc80709ccf151674d65c0c627798d0
Stìophan A. Horne <steven.a.horne@btinternet.com>
2020-06-18 18:17:47 +0000

    removed contact form as due to conflict

--- home/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-class ContactForm(forms.Form):
-    """Form to allow users to contact site"""
-    name = forms.CharField(required=True, max_length=75)
-    email = forms.EmailField(required=True, max_length=75)
-    subject = forms.CharField(required=True, max_length=75)
-    message = forms.CharField(widget=forms.Textarea, required=True, max_length=500)
-
--- home/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from .views import contact
-
-urlpatterns = [
-    # url(r'^index/', index, name="index"),
-    url(r'^contact/', contact, name="contact"),
-]
