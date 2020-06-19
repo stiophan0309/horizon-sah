8f8e8031f758a092eb7d2c1fb313e95ab2ab47cc
Stìophan A. Horne <steven.a.horne@btinternet.com>
2020-06-19 21:08:54 +0000

    updated admin.py

--- custom/views.py
+++ custom/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import Custom
 from .forms import CustomForm
 
+@login_required()
 def create_custom(request):
     if request.method=="POST":
         form = CustomForm(request.POST, request.FILES)
