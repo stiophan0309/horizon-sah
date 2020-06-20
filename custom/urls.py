80df024a524358b3e9466444ea552b85d281422e
Stìophan A. Horne <steven.a.horne@btinternet.com>
2020-06-20 21:00:42 +0000

    Added reverse to view

--- custom/urls.py
+++ custom/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from .views import create_custom
+from .views import request_confirmation
 
 urlpatterns = [
     url(r'^$', create_custom, name="custom"),
+    url(r'^$', request_confirmation, name="request_confirmation"),
 ]
--- custom/views.py
+++ custom/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404, reverse
 from django.contrib.auth.decorators import login_required
 from .models import Custom
 from .forms import CustomForm
+
 
 @login_required()
 def create_custom(request):
@@ -9,11 +10,12 @@ def create_custom(request):
         form = CustomForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect(request_confirmation)
+            return redirect(reverse('request_confirmation'))
     else:
         form = CustomForm()
 
     return render(request, "custom.html", {'form': form})
+
 
 def request_confirmation(request):
     """A view that displays the index page"""
