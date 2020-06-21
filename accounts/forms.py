5ca88c9e9c0e57590855b726745a024eadf00cfe
Stìophan A. Horne <steven.a.horne@btinternet.com>
2020-06-21 19:48:10 +0000

    Migrated data and created view for contact form

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
-from .models import Contact
 
 
 class UserLoginForm(forms.Form):
--- home/views.py
+++ home/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404, reverse
+from django.contrib import messages
+from .models import Contact
+from .forms import ContactForm
 
 # Create your views here.
 def index(request):
     """A view that displays the index page"""
     return render(request, 'index.html')
+
+
+def contact_form(request):
+    if request.method=="POST":
+        form = ContactForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Request submitted")
+            return redirect(reverse('request_confirmation'))
+    else:
+        form = ContactForm()
+
+    return render(request, "index.html#contact", {'form': form})
+
+
+def request_confirmation(request):
+    """A view that displays the index page"""
+    return render(request, 'confirmation.html')
