1df8834b566a2af1998159b1efad5fe648185755
Stìophan A. Horne <steven.a.horne@btinternet.com>
2020-06-23 15:52:24 +0000

    Added landing opage for commissions and reworked urls and views

--- custom/urls.py
+++ custom/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import create_custom
+from .views import create_custom, custom_form
 from .views import request_confirmation
 
 urlpatterns = [
     url(r'^$', create_custom, name="custom"),
-    url(r'^$', request_confirmation, name="request_confirmation"),
+    url(r'^custom/$', custom_form, name="custom_form"),
+    url(r'^custom/$', request_confirmation, name="request_confirmation"),
 ]
--- custom/views.py
+++ custom/views.py
@@ -4,11 +4,14 @@ from django.contrib import messages
 from .models import Custom
 from .forms import CustomForm
 
-
 @login_required()
 def create_custom(request):
+    return render(request, "custom.html")
+
+@login_required()
+def custom_form(request):
     if request.method=="POST":
-        form = CustomForm(request.POST, request.FILES)
+        form = CustomForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, "Request submitted")
@@ -16,7 +19,7 @@ def create_custom(request):
     else:
         form = CustomForm()
 
-    return render(request, "custom.html", {'form': form})
+    return render(request, "custom_form.html", {'form': form})
 
 
 def request_confirmation(request):
