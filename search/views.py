bc7b4ef56d51028e4800a6fc9db86a96f3e3e14e
Stìophan A. Horne <steven.a.horne@btinternet.com>
2020-06-30 22:12:21 +0000

    Added Media query for navbar issues

--- search/views.py
+++ search/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from works.models import Work
 
-# Create a view that perfoems a search in the gallery
+"""Create a view that performs a search in the gallery"""
 def do_search(request):
     works = Work.objects.filter(title__icontains=request.GET['q'])
     return render(request, "works.html", {"works": works})
